d8ba7da6beedf285a7aa4aa7d96533e55d57ca75
David29-ra <davidrivasa.12@gmail.com>
2021-11-20 00:26:33 -0500

    solve some bugs and add all functions

--- scripts/components/taks.js
+++ scripts/components/taks.js
@@ -1,10 +1,13 @@
 export const liTask = function(task) {
-  const imp = task.important ? "important_p" : "important_b"
   const com = task.completed ? "checked" : ""
+  const imp = task.important ? (com? "importantpb" : "important_p") : "important_b"
 
   return `<li>
+            
             <input id = "${task.id}" class="completed" type="checkbox" ${com}/>
-            <p>${task.title}</p>
+            <p class="${com? "colorp" : ""}">${task.title}</p>
             <img id = "${task.id}" class="important" src="./assets/icons/${imp}.svg" alt="importance" />
+            
+            ${task.due_date? `<br><div><span class="${com? "colord" : "date"}">${new Date(task.due_date).toDateString()}</span></div></br>` : ""}
           </li>`
 }
